2ffd06f7cf8f00bc35c266bdbaecd9851a4a1f2c
Anton Kjörck Lindén <anton@noviumdesign.se>
2014-07-22 13:58:10 +0200

    Finished basic nav, added libs

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,1 +1,17 @@
-//Feed me javascript here.
+// Toggle nav
+
+$( ".mainmenu a" ).click(function() {
+  $( "body" ).toggleClass( "popout-nav" );
+});
+
+$( ".mainmenu li" ).click(function() {
+  $( this ).toggleClass( "active" );
+});
+
+// Set width of wrapper
+
+$(".wrap").width($(window).width()-270);
+
+$(window).resize(function(){
+    $(".wrap").width($(window).width()-270);
+});
